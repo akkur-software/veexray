1740ad7cca537dd39eaa0893cabb6954ea3702d5
Alexander  Batyushkin <akkur.software@gmail.com>
2024-10-24 21:09:07 +0600

    Создан пустой console app проект Veex.TestApp, подключен и протестирован Veex.Core

--- _/sources/Veex.Core/Uuid4.h
+++ _/sources/Veex.Core/Uuid4.h
@@ -4,98 +4,102 @@
 #include <stdint.h>
 #include "dllexp.h"
 
-/// <summary>
-/// Размер UUID (в байтах)
-/// </summary>
-constexpr size_t UUID_SIZE = 16;
-
-/// <summary>
-/// Размер строкового представления UUID
-/// </summary>
-constexpr size_t UUID_STRING_SIZE = 36;
-
-/// <summary>
-/// Символ разделителя UUID (дефис)
-/// </summary>
-constexpr char DASH_CHAR = '-';
-
-/// <summary>
-/// Символ, определяющий версию UUID
-/// </summary>
-constexpr char VERSION_CHAR = '4';
-
-/// <summary>
-/// Позиции разделителей в UUID
-/// </summary>
-constexpr std::array<size_t, 4> UUID_DASH_POSITIONS({ 8, 13, 18, 23 });
-
-/// <summary>
-/// Набор значений для конвертации байтов в строковое hex-представление
-/// </summary>
-constexpr std::array<char, 16> HEX_VALUES({ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' });
-
-/// <summary>
-/// Универсальный уникальный идентификатор (UUID) v4
-/// </summary>
-class Uuid4 final
+namespace Veex::Core
 {
-public:
-    /// <summary>
-    /// Предоставляет новый экземпляр Uuid
-    /// </summary>
-    VEEX_CORE_API Uuid4();
 
     /// <summary>
-    /// Предоставляет новый экземпляр Uuid c указанным значением
+    /// Размер UUID (в байтах)
     /// </summary>
-    /// <param name="value">Значение Uuid в виде массива байтов</param>
-    VEEX_CORE_API Uuid4(const std::array<uint8_t, 16>& value);
+    constexpr size_t UUID_SIZE = 16;
 
     /// <summary>
-    /// Предоставляет новый экземпляр Uuid, основанного на значении указанного
+    /// Размер строкового представления UUID
     /// </summary>
-    /// <param name="uuid">Uuid, используемый в качестве исходного значения</param>
-    VEEX_CORE_API Uuid4(const Uuid4& uuid);
-
-    VEEX_CORE_API ~Uuid4() = default;
+    constexpr size_t UUID_STRING_SIZE = 36;
 
     /// <summary>
-    /// Значение идентификатора
+    /// Символ разделителя UUID (дефис)
     /// </summary>
-    std::array<uint8_t, 16> VEEX_CORE_API Value() const;
+    constexpr char DASH_CHAR = '-';
 
     /// <summary>
-    /// Строковое представление идентификатора
+    /// Символ, определяющий версию UUID
     /// </summary>
-    std::string_view VEEX_CORE_API ToString() const;
+    constexpr char VERSION_CHAR = '4';
 
     /// <summary>
-    /// Парсинг строки, содержащей UUID
+    /// Позиции разделителей в UUID
     /// </summary>
-    /// <param name="uuidString">Строка, являющаяся представлением UUID v4</param>
-    /// <returns>Новый экземпляр Uuid</returns>
-    static Uuid4 VEEX_CORE_API  Parse(std::string_view uuidString);
+    constexpr std::array<size_t, 4> UUID_DASH_POSITIONS({ 8, 13, 18, 23 });
 
     /// <summary>
-    /// Равны ли указанные UUID
+    /// Набор значений для конвертации байтов в строковое hex-представление
     /// </summary>
-    friend bool VEEX_CORE_API operator==(const Uuid4& lhs, const Uuid4& rhs);
+    constexpr std::array<char, 16> HEX_VALUES({ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' });
 
     /// <summary>
-    /// Не равны ли указанные UUID
+    /// Универсальный уникальный идентификатор (UUID) v4
     /// </summary>
-    friend bool VEEX_CORE_API operator!=(const Uuid4& lhs, const Uuid4& rhs);
+    class Uuid4 final
+    {
+    public:
+        /// <summary>
+        /// Предоставляет новый экземпляр Uuid
+        /// </summary>
+        VEEX_CORE_API Uuid4();
 
-private:
-    std::array<uint8_t, 8> toByteArray(uint64_t value);
-    std::array<uint8_t, 16> mergeArrays(const std::array<uint8_t, 8>& high, const std::array<uint8_t, 8>& low);
-    std::string stringify(const std::array<uint8_t, 16>& array);
-    std::string byteToHex(uint8_t byte);
+        /// <summary>
+        /// Предоставляет новый экземпляр Uuid c указанным значением
+        /// </summary>
+        /// <param name="value">Значение Uuid в виде массива байтов</param>
+        VEEX_CORE_API Uuid4(const std::array<uint8_t, 16>& value);
 
-    static bool isValid(std::string_view uuidString);
-    static std::array<uint8_t, 16> parse(std::string_view uuidString);    
+        /// <summary>
+        /// Предоставляет новый экземпляр Uuid, основанного на значении указанного
+        /// </summary>
+        /// <param name="uuid">Uuid, используемый в качестве исходного значения</param>
+        VEEX_CORE_API Uuid4(const Uuid4& uuid);
 
-private:
-    std::array<uint8_t, 16> m_value;
-    std::string m_stringValue;
-};
+        VEEX_CORE_API ~Uuid4() = default;
+
+        /// <summary>
+        /// Значение идентификатора
+        /// </summary>
+        std::array<uint8_t, 16> VEEX_CORE_API Value() const;
+
+        /// <summary>
+        /// Строковое представление идентификатора
+        /// </summary>
+        std::string_view VEEX_CORE_API ToString() const;
+
+        /// <summary>
+        /// Парсинг строки, содержащей UUID
+        /// </summary>
+        /// <param name="uuidString">Строка, являющаяся представлением UUID v4</param>
+        /// <returns>Новый экземпляр Uuid</returns>
+        static Uuid4 VEEX_CORE_API  Parse(std::string_view uuidString);
+
+        /// <summary>
+        /// Равны ли указанные UUID
+        /// </summary>
+        friend bool VEEX_CORE_API operator==(const Uuid4& lhs, const Uuid4& rhs);
+
+        /// <summary>
+        /// Не равны ли указанные UUID
+        /// </summary>
+        friend bool VEEX_CORE_API operator!=(const Uuid4& lhs, const Uuid4& rhs);
+
+    private:
+        std::array<uint8_t, 8> toByteArray(uint64_t value);
+        std::array<uint8_t, 16> mergeArrays(const std::array<uint8_t, 8>& high, const std::array<uint8_t, 8>& low);
+        std::string stringify(const std::array<uint8_t, 16>& array);
+        std::string byteToHex(uint8_t byte);
+
+        static bool isValid(std::string_view uuidString);
+        static std::array<uint8_t, 16> parse(std::string_view uuidString);
+
+    private:
+        std::array<uint8_t, 16> m_value;
+        std::string m_stringValue;
+    };
+}
---
+++ _/sources/Veex.TestApp/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sources/Veex.Core/Uuid4.h>
+
+using Veex::Core::Uuid4;
+
+int main()
+{
+    auto uuid1 = Uuid4();
+    auto uuid2 = Uuid4(uuid1);
+
+    auto uuidStr = uuid2.ToString();
+    auto uuid3 = Uuid4::Parse(uuidStr);
+
+    ::printf("UUID4 (1): %s\n", uuid1.ToString().data());
+    ::printf("UUID4 (2): %s\n", uuid2.ToString().data());
+    ::printf("UUID4 (3): %s\n", uuid3.ToString().data());
+
+    return ::getchar();
+}
